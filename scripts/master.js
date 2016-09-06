381b949801460a27e0f558272524a578ead65c0d
Mário Patrão <mariopatrao@gmail.com>
2016-09-06 17:12:38 +0100

    v1

--- scripts/master.js
+++ scripts/master.js
@@ -17,22 +17,27 @@ $(function () {
         $('ul').scrollLeft('500px');
     });*/
 
+    // Bind the swiperightHandler callback function to the swipe event
+    //$('.horizontalRow').on("swiperight", goRight());
 
+    // Bind the swipeleftHandler callback function to the swipe event
+    //$('.horizontalRow').on("swipeleft", goLeft());
 
     $(document).keydown(function (e) {
-        leftPos = $('.horizontalRow').scrollLeft();
-        sectionSize = $('ul li').outerWidth();
+        //leftPos = $('.horizontalRow').scrollLeft();
+        //sectionSize = $('ul li').outerWidth();
         switch (e.which) {
             case 37: // left
-                $('.horizontalRow').animate({ scrollLeft: leftPos - sectionSize }, 800);
+                //('.horizontalRow').animate({ scrollLeft: leftPos - sectionSize }, 800);
+                goLeft();
                 break;
 
             case 38: // up
                 break;
 
             case 39: // right
-                
-                $('.horizontalRow').animate({ scrollLeft: leftPos + sectionSize }, 800);
+                //$('.horizontalRow').animate({ scrollLeft: leftPos + sectionSize }, 800);
+                goRight();
                 break;
 
             case 40: // down
@@ -44,3 +49,17 @@ $(function () {
     });
 
 });
+
+
+function goLeft() {
+    leftPos = $('.horizontalRow').scrollLeft();
+    sectionSize = $('ul li').outerWidth();
+    $('.horizontalRow').animate({ scrollLeft: leftPos - sectionSize }, 800);
+}
+
+
+function goRight() {
+    leftPos = $('.horizontalRow').scrollLeft();
+    sectionSize = $('ul li').outerWidth();
+    $('.horizontalRow').animate({ scrollLeft: leftPos + sectionSize }, 800);
+}

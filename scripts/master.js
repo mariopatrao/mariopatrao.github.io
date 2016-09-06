64ba4ea71a432d8efcd735e5be86dfe68e0e0c5d
Mário Patrão <mariopatrao@gmail.com>
2016-09-06 16:30:05 +0100

    v1

--- scripts/master.js
+++ scripts/master.js
@@ -1,8 +1,46 @@
-$('ul').on('scroll', function () {
-    alert('in');
-    $(document).scrollTo('ul li');
-});
+var leftPos;
+var sectionSize;
 
-$(document).ready(function () {
-    console.log("ready!");
+$(function () {
+
+    $('.horizontalRow').mousewheel(function (event, delta) {
+
+        this.scrollLeft -= (delta * 30);
+
+        event.preventDefault();
+
+    });
+
+    /*$('.horizontalRow').on('scroll', function () {
+        //alert('in');
+        //$(document).scrollTo('.next');
+        $('ul').scrollLeft('500px');
+    });*/
+
+
+
+    $(document).keydown(function (e) {
+        leftPos = $('.horizontalRow').scrollLeft();
+        sectionSize = $('ul li').outerWidth();
+        switch (e.which) {
+            case 37: // left
+                $('.horizontalRow').animate({ scrollLeft: leftPos - sectionSize }, 800);
+                break;
+
+            case 38: // up
+                break;
+
+            case 39: // right
+                
+                $('.horizontalRow').animate({ scrollLeft: leftPos + sectionSize }, 800);
+                break;
+
+            case 40: // down
+                break;
+
+            default: return; // exit this handler for other keys
+        }
+        e.preventDefault(); // prevent the default action (scroll / move caret)
+    });
+
 });

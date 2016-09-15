e0abdee8e55c7fdff2a6cf3824d627d99e9f3e37
Mário Patrão <mariopatrao@gmail.com>
2016-09-15 12:01:13 +0100

    v1

--- scripts/master.js
+++ scripts/master.js
@@ -16,7 +16,7 @@ $(function () {
             scrollAction(direction);
         },
         //Default is 75px, set to 0 for demo so any distance triggers swipe
-        threshold: 0        
+        threshold: 0
     });
 
     // Keyboard pressed
